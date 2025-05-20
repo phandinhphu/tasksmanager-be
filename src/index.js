96e7a8fc6c17fdc0136260548ebb54e39bc3860c
prpjzz <groovemusic4399@pm.me>
2025-05-20 10:01:27 +0700

    Fix limit 100kb json to 10MB

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ app.use(cors({
     credentials: true,
     origin: process.env.FRONTEND_URL || 'http://localhost:3000'
 }))
-app.use(express.json())
+app.use(express.json({ limit: '10mb' }));
 app.use(cookieParser())
 app.use(morgan('dev'))
 app.use(helmet({
